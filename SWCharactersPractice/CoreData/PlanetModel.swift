709137aba33451be97305596cc0a415c3e58fa0c
Михаил Баринов <barmikser95@gmail.com>
2020-02-17 05:23:02 +0300

    HARD_created coredata

---
+++ SWCharactersPractice/CoreData/PlanetModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlanetModel.swift
+//  SWCharactersPractice
+//
+//  Created by Mihail Barinov on 17/02/2020.
+//  Copyright © 2020 Mihail Barinov. All rights reserved.
+//
+
+import Foundation
+import MagicalRecord
+
+class PlanetModel: NSManagedObject {
+
+}
---
+++ SWCharactersPractice/CoreData/VehicleModel.swift
@@ -0,0 +1,14 @@
+//
+//  VehicleModel.swift
+//  SWCharactersPractice
+//
+//  Created by Mihail Barinov on 17/02/2020.
+//  Copyright © 2020 Mihail Barinov. All rights reserved.
+//
+
+import Foundation
+import MagicalRecord
+
+class VehicleModel: NSManagedObject {
+
+}
